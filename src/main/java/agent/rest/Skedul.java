02802b102c9ec1207bf025e539e37dec851adda5
MrRisoni <vanguelisk@gmail.com>
2020-05-16 11:45:00 +0300

    add heroku url

--- src/main/java/agent/rest/Skedul.java
+++ src/main/java/agent/rest/Skedul.java
@@ -1,9 +1,24 @@
 package agent.rest;
 
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
 
+import agent.disk.Partition;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
 
+import com.google.gson.Gson;
 import agent.system.Kernel;
 import agent.utils.FileSystemInfo;
 import agent.utils.SystemInfo;
@@ -21,14 +36,57 @@ public class Skedul {
     }
 
 
-    @Scheduled(fixedRate = 5000)
+   @Scheduled(fixedRate = 5000)
     public void anotherRerouse() {
         Kernel kern = SystemInfo.kernelData();
-        System.out.println(kern);
-    } */
 
+        try {
+            System.out.println(kern);
+
+
+            HttpPost postRequest = new HttpPost("http://localhost:3500/api/zfs/list");
+            Gson gson = new Gson();
+            DefaultHttpClient httpClient = new DefaultHttpClient();
+            StringEntity input = new StringEntity(gson.toJson(kern));
+            System.out.println("GSON TO KRN");
+            System.out.println(gson.toJson(kern));
+
+            input.setContentType("application/json");
+            postRequest.setEntity(input);
+
+            HttpResponse response = httpClient.execute(postRequest);
+
+
+
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+*/
     @Scheduled(fixedRate = 5000)
-    public void zfsData() {
-        System.out.println(FileSystemInfo.getZFSList().get(0));
+    public void anotherRerouse() {
+        ArrayList<Partition> zfsl = FileSystemInfo.getZFSList();
+
+        try {
+            System.out.println(zfsl);
+            HttpPost postRequest = new HttpPost("https://iaveris-api.herokuapp.com/api/zfs/list");
+            Gson gson = new Gson();
+            DefaultHttpClient httpClient = new DefaultHttpClient();
+            StringEntity input = new StringEntity(gson.toJson(zfsl));
+            System.out.println("GSON TO KRN");
+            System.out.println(gson.toJson(zfsl));
+
+            input.setContentType("application/json");
+            postRequest.setEntity(input);
+
+            HttpResponse response = httpClient.execute(postRequest);
+
+
+
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
