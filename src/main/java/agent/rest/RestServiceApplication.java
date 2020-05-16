93caab9f68be36fcca8287bd954df6407abf82f6
MrRisoni <vanguelisk@gmail.com>
2020-05-16 12:01:35 +0300

    no scheduling

--- src/main/java/agent/rest/RestServiceApplication.java
+++ src/main/java/agent/rest/RestServiceApplication.java
@@ -3,9 +3,7 @@ package agent.rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
 //@ConditionalOnProperty(prefix = "agent.rest", name="enabled", havingValue="true", matchIfMissing = false)
 @SpringBootApplication(scanBasePackages = {"agent"})
 public class RestServiceApplication {
